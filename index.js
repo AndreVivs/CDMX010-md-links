a865a427b4c17500eedd54b4e55bf90bc69cae82
AndreVivs <75138276+AndreVivs@users.noreply.github.com>
2021-04-08 14:35:54 -0500

    recursive function

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-module.exports = () => {
-  // ...
-};
+// implementing modules
+const fs = require('fs');
+const pathRq = require('path');
+const argv = require('yargs').argv;
+let route = argv.route;
+
+// Searching .md files
+let searchingMdFiles = (path) => {
+    let mdFiles = [];
+    if(fs.statSync(path).isDirectory()) {
+        fs.readdir(route, 'utf-8', (err, data) => {
+            if(err){
+                console.log('Error: ', err);
+            } else {
+                data.forEach( docOrDir => {
+                    if (fs.statSync(docOrDir).isDirectory()) {
+                        searchingMdFiles(newPath);
+                    } else {
+                        docOrDir == doc
+                        if (pathRq.resolve(docOrDir) !== '.md'){
+                            mdFiles.push.apply(mdFiles, [])
+                        }
+                        return mdFiles.push.apply(mdFiles, [docOrDir]);
+                    }
+                })
+            }
+        })
+    }
+    return mdFiles
+}
+
+console.log(searchingMdFiles(route));
