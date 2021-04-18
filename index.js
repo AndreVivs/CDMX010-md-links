3fbd2164e28f0c7a2d52cf8a13aae96e7ff37e55
AndreVivs <75138276+AndreVivs@users.noreply.github.com>
2021-04-18 01:06:52 -0500

    searchingMdFiles con console y falta agregar el archivo md a mdFiles cuando esta dentro de un directorio

--- index.js
+++ index.js
@@ -2,83 +2,122 @@
 const fs = require('fs');
 const pathRq = require('path');
 const argv = require('yargs').argv;
-let route = 'ArchivosMd'; // argv.route
+let route = argv.route; //'ArchivosMd';
 const fetch = require('node-fetch');
 // variables for get final path
 let getDirection = pathRq.parse(__filename);
 let userDirection = getDirection.dir;
 
-/*
+// //Construyendo el controlador
+// const mdlinks = (option) =>{
+//     switch(option){
+//         case '--validate' || '-v':
+//             console.log('yo ando validando ggg');
+//             break;
+//         case option === '--stats' || '-s':
+//             console.log('sacando tus estadisticas ando you you you');
+//             break;
+//         case option === '--validate' || '-v' && '--stats' || '-s':
+//             console.log('validando y estateando ');
+//             break;
+//         default:
+//             console.log('entregando links');
+//             break;
+//     }
+// }
+// mdlinks(option);
+
 // Searching .md files
-let searchingMdFiles = (path) => {
+let searchingMdFiles = (route) => {
+    console.log('route', route);
     let mdFiles = [];
-    if(fs.statSync(path).isDirectory()) {
-        fs.readdir(route, 'utf-8', (err, data) => {
-            if(err){
-                console.log('Error: ', err);
-            } else {
-                data.forEach( docOrDir => {
-                    if (fs.statSync(docOrDir).isDirectory()) {
-                        searchingMdFiles(route);
-                    } else {
-                        if (pathRq.resolve(docOrDir) === '.md'){
-                          return mdFiles.push.apply(mdFiles, [docOrDir]);
-                        }
-                        
-                    }
-                })
+    if(fs.statSync(route).isDirectory()) {
+        const filesArray = fs.readdirSync(route, 'utf-8')
+        filesArray.forEach( docOrDir => {
+            let newRoute = pathRq.resolve(route, docOrDir)
+            console.log('forEach docOrDir', docOrDir)
+            console.log('filesArray',filesArray);
+            if (fs.statSync(newRoute).isDirectory()) {
+                console.log('el elemento fue directorio');
+                console.log('soy newRoute', newRoute);
+                searchingMdFiles(newRoute);
+            }
+            if (docOrDir.split('.').pop() === 'md') {
+                console.log('archivo md', docOrDir)
+                mdFiles = mdFiles.concat(docOrDir);
+                console.log('mdFiles', mdFiles);
             }
         })
     }
     return mdFiles
 }
-
-console.log(searchingMdFiles(route)); 
---------------------------------------------------------------------------------------*/
+let totalMdFiles = searchingMdFiles(route);
+console.log('yo soy total', totalMdFiles);
 
 // Obtaining finall path
-const getLinks = (userDirection, route, file) =>{
-        let newPath = pathRq.join(userDirection, route, file);
-        const links = fs.readFileSync(newPath,'utf-8').match(/https?:\/\/[a-zA-Z\.\/-]+/gm);
-        return links
-}
+// const getLinks = (userDirection, route, file) =>{
+//         let newPath = pathRq.join(userDirection, route, file);
+//         const links = fs.readFileSync(newPath,'utf-8').match(/https?:\/\/[a-zA-Z\.\/-]+/gm);
+//         return links
+// }
 
-//Validating links
-let ValidatingLinks = (links, file) => {
-    console.log(file);
-    let hrefTxtFile;
-    // Solo ejecutar si el usuario ingreso la opcion --validate sino retornar solo 
-        links.forEach( links => {
-            fetch(links)
-                .then(result => {
-                    hrefTxtFile = {
-                        href: result.url,
-                        text: result.statusText,
-                        status: result.status,
-                        file: file,
-                    };
-                    console.log(hrefTxtFile);
-            })
-            .catch( (error) => (console.error(error)));
-        })
-        return hrefTxtFile
-};
+// //Validating links
+// let validatingLinks = (links, file) => {
+//     console.log(file);
+//     let hrefTxtFile;
+//     // Solo ejecutar si el usuario ingreso la opcion --validate sino retornar solo 
+//         links.forEach( links => {
+//             fetch(links)
+//                 .then(result => {
+//                     hrefTxtFile = {
+//                         href: result.url,
+//                         text: result.statusText,
+//                         status: result.status,
+//                         file: file,
+//                     };
+//                     console.log(hrefTxtFile);
+//             })
+//             .catch( (error) => (console.error(error)));
+//         })
+//         return hrefTxtFile
+// };
 // const linksValidated = ValidatingLinks(links,file);
-// console.log(linksValidated); 
+// console.log(linksValidated);
 
-// Read directory
-let readDirectory = (path)=> {
-    let links = [];
-    if(fs.statSync(path).isDirectory()) {
-        const files = fs.readdirSync(route, 'utf-8')
-        files.forEach( file => {
-            const obtainingLinks = getLinks(userDirection, route, file);
-            links = links.concat(obtainingLinks);
-        })
-    }
-    return links
-};
+// // Read directory
+// let readDirectory = (path)=> {
+//     let links = [];
+//     if(fs.statSync(path).isDirectory()) {
+//         const files = fs.readdirSync(route, 'utf-8')
+//         files.forEach( file => {
+//             const obtainingLinks = getLinks(userDirection, route, file);
+//             links = links.concat(obtainingLinks);
+//         })
+//     }
+//     return links
+// };
 
-const finalLinksArray = readDirectory(route);
-console.log(finalLinksArray);
-ValidatingLinks(finalLinksArray);
+// const finalLinksArray = readDirectory(route);
+// console.log(finalLinksArray);
+// validatingLinks(finalLinksArray);
+
+//  // Stats Links --stats
+//  const linkStatus = (links) => {
+//     console.log('linkStatus', links);
+//     links = links.concat(obtainingLinks);
+//     if(links.lenght <= 0){
+//         // Usar un .lenght para sacar el numero
+//         // de elementos dentro de un arreglo
+//         let totalLinks = links.lenght;
+//         console.log('Total: ', totalLinks)
+//     }
+//     if(links !== links){
+//         // usar filter.lenght para sacar los links que esten repetidos
+//         // y posteriormente obtener la longitud del arreglo para imprimirlo
+//         let uniqueLinks = ++
+//     }
+//     if(result.status <= 400){
+//         // Crear un nuevo arreglo para imprimir la longitud de los links que son <= a 400
+//         let brokenLinks;
+//     }
+// }
